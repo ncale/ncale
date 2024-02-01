2924ead11a9b59566156234e48e207932749e3ae
ncale <nicholasbrodeur@outlook.com>
2024-02-01 15:32:36 -0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col p-6 pt-16">
+      <h1 className="bg-red-200">testing 1 2 3</h1>
+      <h2 className="bg-purple-200">subtitle</h2>
+      <h3 className="bg-green-200">sub subtitle</h3>
+    </main>
+  );
+}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>projecs: testing 1 2 3</h1>
+    </main>
+  );
+}
+  
