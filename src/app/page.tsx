7c2b77fa47caca40028e03868a0dbc53339b4a87
ncale <nicholasbrodeur@outlook.com>
2024-02-01 19:32:51 -0600

    add social icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import portrait from "../../public/IMG_1184.jpeg";
+import arenaIcon from "../../public/arena-mark.svg";
+import fcIcon from "../../public/fc-icon.svg";
+import substackIcon from "../../public/substack-icon.svg";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col p-6 pt-16">
-      <section className="flex flex-row mb-6">
+      <section className="flex flex-row mb-8">
         <div className="mr-4">
           <Image
             src={portrait}
@@ -23,11 +26,20 @@ export default function Home() {
             free to send me an email or Farcaster DM.</p>
         </div>
       </section>
-      <section className="mb-6">
+      <section className="mb-8">
         <ul className="flex flex-row justify-around">
-          <li className="bg-red-200">Farcaster</li>
-          <li className="bg-red-200">Are.na</li>
-          <li className="bg-red-200">Substack</li>
+          <li className="flex align-middle">
+            <Image src={fcIcon} alt="Farcaster Icon" width={18} height={18} className="mr-2"/>
+            Farcaster
+          </li>
+          <li className="flex align-middle">
+            <Image src={arenaIcon} alt="Arena Icon" width={20} height={20} className="mr-2"/>
+            Are.na
+          </li>
+          <li className="flex align-middle">
+            <Image src={substackIcon} alt="Substack Icon" width={15} height={15} className="mr-2"/>
+            Substack
+          </li>
         </ul>
       </section>
     </main>
