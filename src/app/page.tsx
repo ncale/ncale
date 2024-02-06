31a4903dede80ef716e78b19af4cb782854cc69d
ncale <nicholasbrodeur@outlook.com>
2024-02-06 14:51:50 -0600

    made various design tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,84 @@
-import pic from "../../public/profile-pic.jpeg";
+import Image from "next/image";
+// Import social icons
 import arenaIcon from "../../public/arena-mark.svg";
 import fcIcon from "../../public/fc-icon.svg";
 import githubIcon from "../../public/github-mark.svg";
 import substackIcon from "../../public/substack-icon.svg";
-import Image from "next/image";
+// Import image gallery
+import pic1 from "../../public/profile-pic.jpeg";
+import pic2 from "../../public/profile-pic.jpeg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col p-6 pt-16">
+    <main className="flex min-h-screen flex-col">
       <section className="flex flex-row mb-6">
-        <div className="min-h-52 min-w-52 max-h-52 max-w-52 relative mr-4 hidden md:block">
-          <Image
-            src={pic}
-            width={500}
-            height={500}
-            className="rounded"
-            alt="A portrait"
-          />
-        </div>
         <div>
-          <h1 className="text-lg font-medium mb-1">Hi, I'm Nick Brodeur</h1>
+          <h1 className="text-xl font-medium mb-1.5">Hi, I'm Nick Brodeur</h1>
           <p className="text-sm mb-2">I'm an amateur programmer, internet enthusiast, reader, and aspiring writer.</p>
-          <p className="text-sm mb-2">I believe in giving people more sovereignty over their lives. That means I enjoy working on and alongside 
+          <p className="text-sm mb-2">I believe in giving people more sovereignty, and I think technology is one of the surest ways to do that. That means I enjoy working on and alongside 
             systems that are decentralized, transparent, lasting, and simple.</p>
           <p className="text-sm mb-2">Check out my projects if you'd like to see what I've been working on lately. Also, if you'd like to reach me, feel 
-            free to send me an <a href="mailto:nicholasbrodeur@outlook.com" target="_blank">email</a> or 
-            <a href="https://warpcast.com/ncale.eth" target="_blank">Farcaster DM</a>.</p>
+            free to send me an <a href="mailto:nicholasbrodeur@outlook.com" target="_blank" className="underline">email</a> or <a href="https://warpcast.com/ncale.eth" target="_blank" className="underline">Farcaster DM</a>.</p>
+          <p className="text-sm mb-2 font-medium">Socials...</p>
+          <ul className="flex flex-row">
+            <li>
+              <a href="https://warpcast.com/ncale.eth" target="_blank" className="flex align-middle text-sm mr-2">
+                <Image src={fcIcon} alt="Farcaster Icon" width={18} height={18} className="mr-2"/>
+                Farcaster
+              </a>
+            </li>
+            <li>
+              <a href="https://www.are.na/nick-brodeur" target="_blank" className="flex align-middle text-sm mr-3">
+                <Image src={arenaIcon} alt="Arena Icon" width={18} height={18} className="mr-1.5"/>
+                Are.na
+              </a>
+            </li>
+            <li>
+              <a href="https://nickbrodeur.substack.com" target="_blank" className="flex align-middle text-sm mr-2">
+                <Image src={substackIcon} alt="Substack Icon" width={12} height={12} className="mr-2"/>
+                Substack
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/ncale" target="_blank" className="flex align-middle text-sm mr-2">
+                <Image src={githubIcon} alt="Github Icon" width={16} height={16} className="mr-1.5"/>
+                Github
+              </a>
+            </li>
+          </ul>
         </div>
       </section>
       <section className="mb-6">
-        <ul className="flex flex-row justify-around">
-          <li>
-            <a href="https://warpcast.com/ncale.eth" target="_blank" className="flex align-middle text-sm">
-              <Image src={fcIcon} alt="Farcaster Icon" width={18} height={18} className="mr-2"/>
-              Farcaster
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/ncale" target="_blank" className="flex align-middle text-sm">
-              <Image src={githubIcon} alt="Github Icon" width={16} height={16} className="mr-1.5"/>
-              Github
-            </a>
-          </li>
-          <li>
-            <a href="https://www.are.na/nick-brodeur" target="_blank" className="flex align-middle text-sm">
-              <Image src={arenaIcon} alt="Arena Icon" width={18} height={18} className="mr-1.5"/>
-              Are.na
-            </a>
-          </li>
-          <li>
-            <a href="https://nickbrodeur.substack.com" target="_blank" className="flex align-middle text-sm">
-              <Image src={substackIcon} alt="Substack Icon" width={12} height={12} className="mr-2"/>
-              Substack
-            </a>
-          </li>
-        </ul>
+        <h2 className="text-md font-medium mb-1">Image Gallery</h2>
+        <div className="flex flex-row flex-wrap">
+          <div className="min-h-44 min-w-44 max-h-44 max-w-44 relative mr-2 mb-2">
+              <Image
+                src={pic1}
+                width={500}
+                height={500}
+                className="rounded"
+                alt="A portrait"
+              />
+            </div>
+            <div className="min-h-44 min-w-44 max-h-44 max-w-44 relative mr-2 mb-2">
+              <Image
+                src={pic2}
+                width={500}
+                height={500}
+                className="rounded"
+                alt="A portrait"
+              />
+            </div>
+            <div className="min-h-44 min-w-44 max-h-44 max-w-44 relative mr-2 mb-2">
+              <Image
+                src={pic2}
+                width={500}
+                height={500}
+                className="rounded"
+                alt="A portrait"
+              />
+            </div>
+          </div>
       </section>
     </main>
   );
