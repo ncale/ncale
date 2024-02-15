e234e4dd04e2e4d57cf2014d6cf7ac39caf7201e
ncale <nicholasbrodeur@outlook.com>
2024-02-15 15:28:55 -0600

    add project component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,17 @@
-import Image from "next/image";
-import linkIcon from "../../../public/link.svg";
-import githubIcon from "../../../public/github-mark.svg";
+import Project from "@/components/Project";
+
+const projectData = [
+  {
+    title: "Ex. Project Title",
+    desc: "Ex. small description of a project that would fill in this space. Likely the 'About' on Github.",
+    projLink: "",
+    githubLink: "",
+  },
+]
 
 export default function Home() {
+  
+  
   return (
     <main className="flex min-h-screen flex-col">
       <section className="flex flex-row mb-4">
@@ -12,24 +21,7 @@ export default function Home() {
         </div>
       </section>
       <section className="flex flex-col">
-        <div className="">
-          <h2 className="text-md font-bold">Ex. Project Title</h2>
-          <p className="text-sm pb-1">Ex. small description of a project that would fill in this space. Likely the &apos;About&apos; on Github.</p>
-          <ul className="flex flex-col">
-            <li>
-              <a href="" target="_blank" className="text-sm flex items-center w-fit">
-                <Image src={linkIcon} alt="Link to view the site" width={16} height={16} className="mr-1.5" />
-                Link
-              </a>
-            </li>
-            <li>
-              <a href="" target="_blank" className="text-sm flex items-center w-fit">
-                <Image src={githubIcon} alt="Github Icon" width={15} height={15} className="mr-1.5"/>
-                Github
-              </a>
-            </li>
-          </ul>
-        </div>
+        <Project project={projectData[0]} />
       </section>
     </main>
   );
---
+++ src/components/Project.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import linkIcon from "../../../public/link.svg";
+import githubIcon from "../../../public/github-mark.svg";
+
+export default function Project( { project }: { project: { title: string, desc: string, projLink: string, githubLink: string }} ) {
+	return (
+		<div className="">
+			<h2 className="text-md font-bold">{project.title}</h2>
+			<p className="text-sm pb-1">{project.desc}</p>
+			<ul className="flex flex-col">
+				<li>
+					<a href={project.projLink} target="_blank" className="text-sm flex items-center w-fit">
+						<Image src={linkIcon} alt="Link to view the site" width={16} height={16} className="mr-1.5" />
+						Link
+					</a>
+				</li>
+				<li>
+					<a href={project.githubLink} target="_blank" className="text-sm flex items-center w-fit">
+						<Image src={githubIcon} alt="Github Icon" width={15} height={15} className="mr-1.5"/>
+						Github
+					</a>
+				</li>
+			</ul>
+		</div>
+	)
+}
