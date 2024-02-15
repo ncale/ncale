2e28eb3942ee754cf7dd8239602802820ddf6eec
ncale <nicholasbrodeur@outlook.com>
2024-02-15 15:42:04 -0600

    update project styles

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -4,19 +4,19 @@ import githubIcon from "../../public/github-mark.svg"
 
 export default function Project( { project }: { project: { title: string, desc: string, projLink: string, githubLink: string }} ) {
 	return (
-		<div className="">
-			<h2 className="text-md font-bold">{project.title}</h2>
-			<p className="text-sm pb-1">{project.desc}</p>
-			<ul className="flex flex-col">
+		<div className="mb-5">
+			<h3 className="text-md font-bold leading-tight">{project.title}</h3>
+			<p className="text-sm pb-1 leading-tight">{project.desc}</p>
+			<ul className="flex">
 				<li>
 					<a href={project.projLink} target="_blank" className="text-sm flex items-center w-fit">
 						<Image src={linkIcon} alt="Link to view the site" width={16} height={16} className="mr-1.5" />
 						Link
 					</a>
 				</li>
-				<li>
+				<li className="ml-2">
 					<a href={project.githubLink} target="_blank" className="text-sm flex items-center w-fit">
-						<Image src={githubIcon} alt="Github Icon" width={15} height={15} className="mr-1.5"/>
+						<Image src={githubIcon} alt="Github Icon" width={15} height={15} className="mr-1"/>
 						Github
 					</a>
 				</li>
