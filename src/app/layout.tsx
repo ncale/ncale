5a1069c2cbf77a7a0b55ec9c88eccc87b5b49c63
ncale <nicholasbrodeur@outlook.com>
2024-03-03 15:38:24 -0700

    fix sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,12 +35,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`box-border ${font.className} bg-white`}>
-        <div className="bg-white flex flex-col items-center md:flex-row md:items-start md:justify-center"> {/* items-center */}
+        <div className="flex flex-col items-center md:flex-row md:items-start md:justify-start md:px-20">
           
           {/* Side Bar */}
-          <header className="w-11/12 pt-8 md:w-40 md:pt-12 md:h-screen">
+          <header className="fixed top-0 w-11/12 pt-6 md:w-40 md:pt-12 md:h-screen bg-white">
             <div className="bg-rainbow-gradient p-4 pt-3 rounded flex justify-between md:flex-col md:justify-start">
-              <div className="font-bold w-20 md:mb-1">nickbrodeur.com / ncale.eth</div> {/*  mb-1 */}
+              <div className="font-bold w-20 md:mb-1">nickbrodeur.com / ncale.eth</div>
               <ul className="flex flex-row md:flex-col">
                 <li className="ml-2 md:ml-0"><Link href="/">home</Link></li>
                 <li className="ml-2 md:ml-0"><Link href="projects">projects</Link></li>
@@ -49,7 +49,7 @@ export default function RootLayout({
           </header>  
               
           {/* Main Content */}
-          <div className="w-11/12 p-2 pt-4 md:w-8/12 md:p-6 md:pt-12">
+          <div className="w-11/12 p-2 pt-4 mt-24 md:w-full md:p-6 md:pt-12 md:ml-40 md:mt-0">
             {children}
           </div>
 
