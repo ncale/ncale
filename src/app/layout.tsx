0856ac39e9f3ef0bf5980d4102c882dabf353eaf
ncale <nicholasbrodeur@outlook.com>
2024-03-03 15:39:03 -0700

    rename sidebar to navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
       <body className={`box-border ${font.className} bg-white`}>
         <div className="flex flex-col items-center md:flex-row md:items-start md:justify-start md:px-20">
           
-          {/* Side Bar */}
+          {/* Navbar */}
           <header className="fixed top-0 w-11/12 pt-6 md:w-40 md:pt-12 md:h-screen bg-white">
             <div className="bg-rainbow-gradient p-4 pt-3 rounded flex justify-between md:flex-col md:justify-start">
               <div className="font-bold w-20 md:mb-1">nickbrodeur.com / ncale.eth</div>
