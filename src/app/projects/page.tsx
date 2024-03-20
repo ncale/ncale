623820369de5f0f40e9965a54646aa8f0ce63bc1
ncale <nicholasbrodeur@outlook.com>
2024-03-20 19:58:52 +0000

    add new project data

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,8 +2,15 @@ import Project from "@/components/Project";
 
 const projectData = [
   {
+    title: "Onchain Clarity Home Site",
+    desc: "Home site for Onchain Clarity Co., a web3 brand with the express goal of increasing onchain visibilty for everyone",
+    projDomain: "onchainclarity.co",
+    projLink: "https://onchainclarity.co",
+    githubLink: "https://github.com/ncale/onchain-clarity",
+  },
+	{
     title: "BasedFriends",
-    desc: "See when your friends are online or onchain with BasedFriends - a web app that integrates with Farcaster to build a personalized friends list",
+    desc: "Web app that shows when your friends are online or onchain. BasedFriends integrates with Farcaster to build a personalized friends list",
     projDomain: "basedfriends.xyz",
     projLink: "https://basedfriends.xyz",
     githubLink: "https://github.com/ncale/based-friends",
