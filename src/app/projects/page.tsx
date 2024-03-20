da025ef42ce7cf97762451a49cbeb2b791cbbba7
ncale <nicholasbrodeur@outlook.com>
2024-03-20 20:05:00 +0000

    move data to separate file

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,22 +1,5 @@
 import Project from "@/components/Project";
-
-const projectData = [
-  {
-    title: "Onchain Clarity Home Site",
-    desc: "Home site for Onchain Clarity Co., a web3 brand with the express goal of increasing onchain visibilty for everyone",
-    projDomain: "onchainclarity.co",
-    projLink: "https://onchainclarity.co",
-    githubLink: "https://github.com/ncale/onchain-clarity",
-  },
-	{
-    title: "BasedFriends",
-    desc: "Web app that shows when your friends are online or onchain. BasedFriends integrates with Farcaster to build a personalized friends list",
-    projDomain: "basedfriends.xyz",
-    projLink: "https://basedfriends.xyz",
-    githubLink: "https://github.com/ncale/based-friends",
-    demoLink: "https://www.loom.com/share/e509901fbd374e38a0bfee293462a216"
-  },
-]
+import { projectData } from "@/data";
 
 export default function Home() {
   
