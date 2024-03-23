1a33406d53cf9e669fa908baf6d8132fb48dd0bc
ncale <nicholasbrodeur@outlook.com>
2024-03-23 11:51:23 -0500

    move navbar to a separate component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import Link from "next/link";
 import { Nunito } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar"
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -39,15 +39,7 @@ export default function RootLayout({
         <div className="flex flex-col items-center md:flex-row md:items-start md:justify-start md:px-20">
           
           {/* Navbar */}
-          <header className="fixed top-0 w-11/12 pt-6 md:w-40 md:pt-12 md:h-screen bg-white">
-            <div className="bg-rainbow-gradient p-4 pt-3 rounded flex justify-between md:flex-col md:justify-start">
-              <div className="font-bold w-20 md:mb-1">nickbrodeur.com / ncale.eth</div>
-              <ul className="flex flex-row md:flex-col">
-                <li className="ml-2 md:ml-0"><Link href="/">home</Link></li>
-                <li className="ml-2 md:ml-0"><Link href="projects">projects</Link></li>
-              </ul>
-            </div>
-          </header>  
+          <Navbar />
               
           {/* Main Content */}
           <div className="w-11/12 p-2 pt-4 mt-24 md:w-full md:p-6 md:pt-12 md:ml-40 md:mt-0">
