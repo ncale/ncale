f41dbfda8333ad396f39a8040b38df3c10a49004
ncale <nicholasbrodeur@outlook.com>
2024-03-26 16:04:48 -0500

    update styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,33 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar"
+import Navbar from "@/components/navbar";
 
 const font = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Nick Brodeur',
-  description: 'Hi, I\'m Nick Brodeur. I\'m an amateur programmer, internet enthusiast...',
-  authors: [{ name: 'Nick Brodeur' }],
-  keywords: ['Nick', 'Brodeur', 'developer', 'crypto', 'web3'],
-  metadataBase: new URL('https://nickbrodeur.com'),
+  title: "Nick Brodeur",
+  description:
+    "Hi, I'm Nick Brodeur. I'm an amateur programmer, internet enthusiast...",
+  authors: [{ name: "Nick Brodeur" }],
+  keywords: ["Nick", "Brodeur", "developer", "crypto", "web3"],
+  metadataBase: new URL("https://nickbrodeur.com"),
   openGraph: {
-    title: 'Nick Brodeur',
-    description: 'Hi, I\'m Nick Brodeur. I\'m an amateur programmer, internet enthusiast...',
-    url: 'https://nickbrodeur.com',
-    siteName: 'Nick Brodeur',
+    title: "Nick Brodeur",
+    description:
+      "Hi, I'm Nick Brodeur. I'm an amateur programmer, internet enthusiast...",
+    url: "https://nickbrodeur.com",
+    siteName: "Nick Brodeur",
     images: [
       {
-        url: '',
+        url: "",
         width: 500,
-        height: 500
-      }
+        height: 500,
+      },
     ],
-    locale: 'en_US',
-    type: 'website',
-  }
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
@@ -35,17 +37,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`box-border ${font.className} bg-white`}>
+      <body className={`box-border ${font.className} bg-paper`}>
         <div className="flex flex-col items-center md:flex-row md:items-start md:justify-start md:px-20">
-          
           {/* Navbar */}
           <Navbar />
-              
+
           {/* Main Content */}
           <div className="w-11/12 p-2 pt-4 mt-24 md:w-full md:p-6 md:pt-12 md:ml-40 md:mt-0">
             {children}
           </div>
-
         </div>
       </body>
     </html>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,20 +4,29 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function Navbar() {
-	const currentPath = usePathname()
-	return (
-		<header className="fixed top-0 w-11/12 pt-6 md:w-40 md:pt-12 md:h-screen bg-white">
-			<div className="bg-rainbow-gradient p-4 pt-3 rounded flex justify-between md:flex-col md:justify-start">
-				<div className="font-bold w-20 md:mb-1">nickbrodeur.com / ncale.eth</div>
-				<ul className="flex flex-row md:flex-col">
-					<li className="ml-2 md:ml-0">
-						<Link href="/" className={currentPath === "/" ? "font-bold" : ""}>home</Link>
-					</li>
-					<li className="ml-2 md:ml-0">
-						<Link href="/projects" className={currentPath === "/projects" ? "font-bold" : ""}>projects</Link>
-					</li>
-				</ul>
-			</div>
-		</header>  
-	)
+  const currentPath = usePathname();
+  return (
+    <header className="fixed top-0 w-11/12 mt-6 md:w-40 md:mt-12 md:h-screen bg-paperTransparent">
+      <div className="bg-rainbow-gradient p-4 pt-3 rounded flex justify-between md:flex-col md:justify-start shadow-lg">
+        <div className="font-bold w-20 md:mb-1">
+          nickbrodeur.com / ncale.eth
+        </div>
+        <ul className="flex flex-row md:flex-col">
+          <li className="ml-2 md:ml-0">
+            <Link href="/" className={currentPath === "/" ? "font-bold" : ""}>
+              home
+            </Link>
+          </li>
+          <li className="ml-2 md:ml-0">
+            <Link
+              href="/projects"
+              className={currentPath === "/projects" ? "font-bold" : ""}
+            >
+              projects
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 }
