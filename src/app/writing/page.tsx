0a520f7442f70eabab5e0ec51d8e59a42d6d1c83
ncale <nicholasbrodeur@outlook.com>
2024-04-28 17:00:19 -0500

    update writing page to match spacing on other pages

--- src/app/writing/page.tsx
+++ src/app/writing/page.tsx
@@ -1,12 +1,15 @@
 export default function Home() {
   return (
-    <main className="">
-      <section className="space-y-4">
+    <main className="flex flex-col space-y-8">
+      <section className="space-y-5">
         <h1 className="font-app-special text-2xl">Writing</h1>
         <p>
           My life goal is to write one brilliant sentence. Until then, this will
           probably be a lot of nonsense.
         </p>
+      </section>
+      <section className="flex flex-col space-y-8">
+        <p>...</p>
       </section>
     </main>
   );
