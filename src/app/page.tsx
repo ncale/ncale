1635bea85d56e77a6cf80b63a615ed69927a6ae0
ncale <nicholasbrodeur@outlook.com>
2024-04-28 12:51:06 -0500

    split image gallery to a separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import Image from "next/image";
-import pic1 from "@/../public/images/profile-pic.jpeg";
-import pic2 from "@/../public/images/pic2.jpeg";
-import pic3 from "@/../public/images/pic3.jpeg";
 import SocialLinks from "@/components/SocialLinks";
-
-const imgList = [pic1, pic2, pic3];
+import ImageGallery from "@/components/image-gallery";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
-      <section className="mb-4 flex flex-row md:w-[32em]">
+      <section className="mb-4 flex flex-row">
         <div>
           <h1 className="mb-2 font-app-special text-xl font-medium">
             Hi, I&apos;m Nick Brodeur
@@ -50,27 +45,7 @@ export default function Home() {
       <section className="mb-5">
         <SocialLinks />
       </section>
-      <section className="mb-5">
-        <h2 className="text-md mb-1 font-app-special font-semibold">
-          Image Gallery
-        </h2>
-        <div className="flex flex-row flex-wrap">
-          {imgList.map((pic, i) => (
-            <div
-              key={i}
-              className="relative mb-2 mr-2 h-fit min-h-44 min-w-44 max-w-44 overflow-hidden shadow-md"
-            >
-              <Image
-                src={pic}
-                width={500}
-                height={500}
-                className="absolute left-1/2 min-h-44 -translate-x-1/2 rounded"
-                alt="A portrait"
-              />
-            </div>
-          ))}
-        </div>
-      </section>
+      <ImageGallery />
     </main>
   );
 }
