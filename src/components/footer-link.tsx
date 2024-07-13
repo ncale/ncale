4e66ab6cc553324758595894615154caced82f97
ncale <nicholasbrodeur@outlook.com>
2024-07-13 00:25:31 -0500

    Add underline to footer links

--- src/components/footer-link.tsx
+++ src/components/footer-link.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 export function FooterLink(props: { href: string; children: ReactNode }) {
   return (
-    <p className="text-xs">
+    <p className="text-xs hover:underline">
       <a href={props.href} target="_blank">
         {props.children}
       </a>
