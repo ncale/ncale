05fee343a28868d4cfa98f120bbc17203bb3ce61
ncale <nicholasbrodeur@outlook.com>
2024-07-12 23:28:21 -0500

    Add link to projects and reorder

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,17 +1,24 @@
 import { Card, CardContent, CardHeader } from "./ui/card";
 
-export function Project(props: { name: string; bio: string; date: string }) {
+export function Project(props: {
+  name: string;
+  link: string;
+  bio: string;
+  date: string;
+}) {
   return (
-    <Card>
-      <CardHeader>
-        <div className="flex justify-between">
-          <h4 className="font-medium">{props.name}</h4>
-          <p className="text-muted-foreground">{props.date}</p>
-        </div>
-      </CardHeader>
-      <CardContent>
-        <p className="text-sm">{props.bio}</p>
-      </CardContent>
-    </Card>
+    <a href={props.link} target="_blank" className="block">
+      <Card className="hover:bg-muted">
+        <CardHeader>
+          <div className="flex justify-between">
+            <h4 className="font-medium">{props.name}</h4>
+            <p className="text-muted-foreground">{props.date}</p>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <p className="text-sm">{props.bio}</p>
+        </CardContent>
+      </Card>
+    </a>
   );
 }
