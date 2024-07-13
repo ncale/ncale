ebdb21b1d562d3d58720fd344a4819436009a954
ncale <nicholasbrodeur@outlook.com>
2024-07-12 23:20:51 -0500

    Add project component

---
+++ src/components/project.tsx
@@ -0,0 +1,17 @@
+import { Card, CardContent, CardHeader } from "./ui/card";
+
+export function Project(props: { name: string; bio: string; date: string }) {
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex justify-between">
+          <h4 className="font-medium">{props.name}</h4>
+          <p className="text-muted-foreground">{props.date}</p>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <p className="text-sm">{props.bio}</p>
+      </CardContent>
+    </Card>
+  );
+}
