c5e4ce87db8d4d14bc96fd558efb31947eddb5b3
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-10-08 13:40:14 -0500

    chore: add all links to a constants file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,25 @@
+/** Links */
+
+// Header / Footer / General
+
 export const WEBSITE_URL = "https://nickbrodeur.com";
 export const LICENSE_URL = "https://github.com/ncale/ncale/blob/master/LICENSE";
+
+export const TELEGRAM_URL = "https://t.me/nbrodeur";
+export const EMAIL_LINK = "mailto:nicholasbrodeur@outlook.com";
+
+export const GITHUB_URL = "https://github.com/ncale";
+export const SUBSTACK_URL = "https://nickbrodeur.substack.com";
+
+export const TWITTER_URL = "https://twitter.com/nick_brodeur";
+export const FARCASTER_URL = "https://warpcast.com/ncale.eth";
+export const ARENA_URL = "https://www.are.na/nick-brodeur/channels";
+
+// Projects
+
+export const NAMESTONE_SDK_URL = "https://www.npmjs.com/package/namestone-sdk";
+export const JELLY_BEANS_URL = "https://jellybeans.wtf/";
+export const FIGHT_IQ_URL = "https://fightiq.io/";
+export const WANNABET_NAMES_URL = "https://names.wannabet.cc/";
+export const WANNABET_URL = "https://wannabet.cc/";
+export const GOVSCORE_URL = "https://opgovscore.com/delegates";
