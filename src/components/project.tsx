b5ac3bf60e8461ccb6727b42392183be7bae4718
Nick Brodeur <106794061+ncale@users.noreply.github.com>
2024-10-08 14:52:58 -0500

    chore: update projects to have a dropdown

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,24 +1,57 @@
+import { useState } from "react";
 import { Card, CardContent, CardHeader } from "./ui/card";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { cn } from "@/lib/utils";
+import { Badge } from "./ui/badge";
 
 export function Project(props: {
   name: string;
   link: string;
-  bio: string;
+  githubLink?: string;
   date: string;
+  desc: string;
+  details: string;
 }) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <a href={props.link} target="_blank" className="block">
-      <Card className="hover:bg-muted">
+    <div className="bg-muted rounded-xl mb-4">
+      <Card className="cursor-pointer" onClick={() => setOpen(!open)}>
         <CardHeader>
           <div className="flex justify-between">
             <h4 className="font-medium">{props.name}</h4>
-            <p className="text-muted-foreground">{props.date}</p>
+            <div className="flex items-center gap-x-1">
+              <p className="text-muted-foreground">{props.date}</p>
+              <button>
+                <ChevronDownIcon
+                  className={cn("h-4 w-4 transition-transform duration-200", open ? "rotate-180" : "")}
+                />
+              </button>
+            </div>
           </div>
         </CardHeader>
         <CardContent>
-          <p className="text-sm">{props.bio}</p>
+          <p className="text-sm">{props.desc}</p>
         </CardContent>
       </Card>
-    </a>
+      <div className={cn("overflow-hidden transition-all duration-200", open ? "max-h-96" : "max-h-0")}>
+        <CardContent className="px-4 py-2 space-y-1.5">
+          <p className="text-sm">{props.details}</p>
+          <div className="flex items-center justify-between">
+            <div className="space-x-1">
+              <a href={props.link} target="_blank">
+                <Badge>Link</Badge>
+              </a>
+              {props.githubLink && (
+                <a href={props.githubLink} target="_blank">
+                  <Badge>Github</Badge>
+                </a>
+              )}
+            </div>
+            {props.githubLink && <span className="inline-block text-muted-foreground text-xs">MIT License</span>}
+          </div>
+        </CardContent>
+      </div>
+    </div>
   );
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -18,8 +18,19 @@ export const ARENA_URL = "https://www.are.na/nick-brodeur/channels";
 // Projects
 
 export const NAMESTONE_SDK_URL = "https://www.npmjs.com/package/namestone-sdk";
+export const NAMESTONE_SDK_GITHUB_URL = "https://github.com/resolverworks/namestone-sdk";
+
 export const JELLY_BEANS_URL = "https://jellybeans.wtf/";
+export const JELLY_BEANS_GITHUB_URL = "https://github.com/ncale/jellybeans-website";
+
 export const FIGHT_IQ_URL = "https://fightiq.io/";
+export const FIGHT_IQ_GITHUB_URL = "https://github.com/ncale/fight-iq-web-app";
+
 export const WANNABET_NAMES_URL = "https://names.wannabet.cc/";
+export const WANNABET_NAMES_GITHUB_URL = "https://github.com/wannabet-cc/wannabet-names";
+
 export const WANNABET_URL = "https://wannabet.cc/";
+export const WANNABET_GITHUB_URL = "https://github.com/wannabet-cc/wannabet";
+
 export const GOVSCORE_URL = "https://opgovscore.com/delegates";
+export const GOVSCORE_GITHUB_URL = "https://github.com/ncale/optimism-gov-score";
