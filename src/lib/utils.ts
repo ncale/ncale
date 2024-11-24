5d0f94fe44da82a74603ef1d83faba0dd60a68c4
Nick Brodeur <nicholasbrodeur@outlook.com>
2024-11-24 17:12:26 -0600

    chore: update `updated_at` display for arena channels

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getDaysAgo(date: string | Date): string {
+  const diffTime = Math.abs(new Date().getTime() - new Date(date).getTime());
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  return `${diffDays} ${diffDays === 1 ? "day" : "days"} ago`;
 }
